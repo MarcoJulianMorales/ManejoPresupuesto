f5841e4fa2073eb7336a4a0351cc7c74bab17e5f
Marco Julian Morales Leyva <marcojulian.morales@gmail.com>
2023-08-26 23:15:22 -0700

    Video 110 terminado, insert a TiposCuentas

---
+++ ManejoPresupuesto/Controllers/TiposCuentaController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models.TiposCuentas;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentaController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentaController(IRepositorioTiposCuentas repositorioTiposCuentas ) 
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+        }
+
+        public IActionResult Crear()
+        {
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            tipoCuenta.UsuarioId = 1;
+            repositorioTiposCuentas.Crear( tipoCuenta );
+
+            return View();
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/TiposCuentas/TipoCuenta.cs
@@ -0,0 +1,29 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models.TiposCuentas
+{
+    public class TipoCuenta//: IValidatableObject
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La longitud de Nombre debe ser entre 3 y 50 caracteres")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set;}
+        public int Orden { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+                
+        //        if(primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayúscula..", new[] {nameof(Nombre)});
+        //        }
+        //    }
+        //}
+    }
+}
