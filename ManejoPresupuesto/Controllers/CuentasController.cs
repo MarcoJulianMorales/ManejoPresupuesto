119ee3501d82ff7109ee9997d3f9dcbda35c305e
Marco Julian Morales Leyva <marcojulian.morales@gmail.com>
2023-08-28 13:16:10 -0700

    Video 129, aislamiento de css

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,75 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+        private readonly IRepositorioCuentas repositorioCuentas;
+
+        public CuentasController(
+            IRepositorioTiposCuentas repositorioTiposCuentas, 
+            IServicioUsuarios servicioUsuarios,
+            IRepositorioCuentas repositorioCuentas)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+            this.repositorioCuentas = repositorioCuentas;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var cuentasConTipoCuenta = await repositorioCuentas.Buscar(usuarioId);
+
+            var modelo = cuentasConTipoCuenta
+                .GroupBy(x => x.TipoCuenta)
+                .Select(grupo => new IndiceCuentasDTO()
+                {
+                    TipoCuenta = grupo.Key,
+                    Cuentas = grupo.AsEnumerable()
+                }).ToList();
+
+            return View(modelo);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var modelo = new CuentaCreacionDTO();
+            modelo.TiposCuentas = await ObtenerTiposCuentas(usuarioId);
+
+            return View(modelo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(CuentaCreacionDTO cuentaCreacionDTO)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = repositorioTiposCuentas.Obtener(usuarioId);
+
+            if(tipoCuenta == null)
+                return RedirectToAction("NoEncontrado", "Home");
+
+            if (!ModelState.IsValid)
+            {
+                cuentaCreacionDTO.TiposCuentas = await ObtenerTiposCuentas(usuarioId);
+                return View(cuentaCreacionDTO);
+            }
+
+            await repositorioCuentas.Crear(cuentaCreacionDTO);
+            return RedirectToAction("Index");
+        }
+
+        private async Task<IEnumerable<SelectListItem>> ObtenerTiposCuentas(int usuarioId)
+        {
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            return tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/CuentaCreacionDTO.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models
+{
+    public class CuentaCreacionDTO: Cuenta
+    {
+        public IEnumerable<SelectListItem> TiposCuentas { get; set; }
+    }
+}
