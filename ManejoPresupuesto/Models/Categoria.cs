cbb59ab7ef0dca6b581f6f5f1443feee880ea2a3
Marco Julian Morales Leyva <marcojulian.morales@gmail.com>
2023-08-28 16:36:13 -0700

    Video 137 terminado, catalogo Categorias terminado

---
+++ ManejoPresupuesto/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        [StringLength(maximumLength:50, ErrorMessage = "No puede ser mayor a 50 caracteres")]
+        public string Nombre { get; set; }
+        [Display(Name ="Tipo de Operacion")]
+        public TipoOperacion TipoOperacionId { get; set; }
+        public int UsuarioId { get; set; }
+
+    }
+}
